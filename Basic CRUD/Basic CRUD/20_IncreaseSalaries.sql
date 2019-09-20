ff19da10c0faedd5dcded3a85c69abd28b3e6f7e
Todor <chapkanov.t@gmail.com>
2019-09-20 23:13:08 +0300

    Some missing query added

--- Basic CRUD/Basic CRUD/20_IncreaseSalaries.sql
+++ Basic CRUD/Basic CRUD/20_IncreaseSalaries.sql
@@ -5,3 +5,9 @@ TO DISC = 'C:\SoftUni.BAK'
 WITH FORMAT,
 [MEDIANAME] = 'C_',
 [NAME] = 'Full backup on SoftUni database'
+
+UPDATE Employees
+SET Salary = Salary * 1.12
+WHERE DepartmentId IN(1,2,4,11)
+
+SELECT Salary FROM Employees
